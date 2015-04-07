ab7507899e96ccc5b552f820e384a5e8409782fe
cyrilzhao <cyrilzhao@qq.com>
2015-04-07 16:54:18 +0800

    modified components

--- app/views/components/rewardtaskheader.blade.php
+++ app/views/components/rewardtaskheader.blade.php
@@ -1,5 +1,9 @@
 <div class="reward-task-header">
 	<p class="title">悬赏任务</p>
 	<p class="content">直接给出选上价格及方案来招标，限时且不议价</p>
-	<p class="content step">1.填写任务&nbsp2.资金托管&nbsp3.确认发布</p>
+	<p class="content step">
+		<span>1.填写任务</span>
+		<span>2.资金托管</span>
+		<span>3.确认发布</span>
+	</p>
 </div>
