3e85d23a36f66903a3a4affafd649c08a50574c8
cyrilzhao <cyrilzhao@qq.com>
2015-04-08 14:46:32 +0800

    merge code

--- app/views/auctions/step-4.blade.php
+++ app/views/auctions/step-4.blade.php
@@ -16,19 +16,72 @@
     @include('components.bountyhunterleft')
     @include('components.auctionheader')
     
-    <div class="step2 auctionstep">
+    <div class="step4 auctionstep">
         <form>
             <ul>
                 <li>
-                    <label class="label">任务主题：</label>
-                    <span>广州紫睿科技有限公司</span>
+                    <label class="label">任务宝账户：</label>
+                    <span>Vivine</span>
                 </li>
-                
+                <li>
+                    <label class="label">充值用途：</label>
+                    <span>为全款悬赏任务<span>［922928］给公司起名</span> 充值</span>
+                </li>
+                <li>
+                    <label class="label">当前账户余额：</label>
+                    <span>7000 铜币 200银币 0金币</span>
+                </li>
+                <li>
+                    <label class="label">任务所需金额：</label>
+                    <span>1000 元</span>
+                </li>
+                <li>
+                    <label class="label">还需充值金额：</label>
+                    <span>1000 元</span>
+                </li>
+                <li>
+                    <label class="label">充值渠道：</label>
+                    <div class="banks">
+                        <div class="bank-item">
+                            <input type="radio" id= "cb" name="bank">
+                            <label for="cb"><img src="/images/rewardtask/cb.gif" alt="cb" class="bank" width="150" height="30" /></label>
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "ccb" name="bank">
+                            <label for="ccb"><img src="/images/rewardtask/ccb.gif" alt="ccb" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "cgb" name="bank">
+                            <label for="cgb"><img src="/images/rewardtask/cgb.gif" alt="cgb" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "icbc" name="bank">
+                            <label for="icbc"><img src="/images/rewardtask/icbc.gif" alt="icbc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "abc" name="bank">
+                            <label for="abc"><img src="/images/rewardtask/abc.gif" alt="abc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "ibc" name="bank">
+                            <label for="ibc"><img src="/images/rewardtask/ibc.gif" alt="ibc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "sbc" name="bank">
+                            <label for="sbc"><img src="/images/rewardtask/sbc.gif" alt="sbc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "alipay" name="bank">
+                            <label for="alipay"><img src="/images/rewardtask/alipay.gif" alt="alipay" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </li>
             </ul>
         </form>
         <div class="next-step">
             <a href="/auction/step-3" class="btn last">上一步</a>
-            <a href="/auction/step-5" class="btn next">现在支付</a>
+            <a href="/auction/step-5" class="btn next">下一步</a>
         </div>
     </div>
 </div>
--- app/views/rewardtasks/step-3.blade.php
+++ app/views/rewardtasks/step-3.blade.php
@@ -15,50 +15,68 @@
     @include('components.bountyhunterleft')
     @include('components.rewardtaskheader')
     <div class="step3 rewardtaskstep">
-        <div class="detail">
-            <p><label>任务宝账户: </label>&nbsp<span>Vivine</span></p>
-            <p><label>充值用途: </label>&nbsp为全款悬赏任务 [<span>922928</span>] 给公司起名 充值</p>
-            <p><label>当前账户余额: </label>&nbsp<span>700铜币</span> <span>200银币</span> <span>0金币</span></p>
-            <p><label>任务所需金额: </label>&nbsp<span>1000</span>元</p>
-            <p><label>还需充值金额: </label>&nbsp<span>1000</span>元</p>
-            <p><label>充值渠道:</label></p>
-            <form>
-                <ul>
-                    <li>
-                        <input type="radio" id= "cb" name="cb">
-                        <img src="/images/rewardtask/cb.gif" alt="cb" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "ccb" name="ccb">
-                        <img src="/images/rewardtask/ccb.gif" alt="ccb" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "cgb" name="cgb">
-                        <img src="/images/rewardtask/cgb.gif" alt="cgb" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "icbc" name="icbc">
-                        <img src="/images/rewardtask/icbc.gif" alt="icbc" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "abc" name="abc">
-                        <img src="/images/rewardtask/abc.gif" alt="abc" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "ibc" name="ibc">
-                        <img src="/images/rewardtask/ibc.gif" alt="ibc" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "sbc" name="sbc">
-                        <img src="/images/rewardtask/sbc.gif" alt="sbc" class="bank" width="150" height="30" />   
-                    </li>
-                    <li>
-                        <input type="radio" id= "alipay" name="alipay">
-                        <img src="/images/rewardtask/alipay.gif" alt="alipay" class="bank" width="150" height="30" />   
-                    </li>
-                </ul>
-            </form>
-        </div>
+        <form>
+            <ul>
+                <li>
+                    <label class="label">任务宝账户：</label>
+                    <span>Vivine</span>
+                </li>
+                <li>
+                    <label class="label">充值用途：</label>
+                    <span>为全款悬赏任务<span>［922928］给公司起名</span> 充值</span>
+                </li>
+                <li>
+                    <label class="label">当前账户余额：</label>
+                    <span>7000 铜币 200银币 0金币</span>
+                </li>
+                <li>
+                    <label class="label">任务所需金额：</label>
+                    <span>1000 元</span>
+                </li>
+                <li>
+                    <label class="label">还需充值金额：</label>
+                    <span>1000 元</span>
+                </li>
+                <li>
+                    <label class="label">充值渠道：</label>
+                    <div class="banks">
+                        <div class="bank-item">
+                            <input type="radio" id= "cb" name="bank">
+                            <label for="cb"><img src="/images/rewardtask/cb.gif" alt="cb" class="bank" width="150" height="30" /></label>
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "ccb" name="bank">
+                            <label for="ccb"><img src="/images/rewardtask/ccb.gif" alt="ccb" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "cgb" name="bank">
+                            <label for="cgb"><img src="/images/rewardtask/cgb.gif" alt="cgb" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "icbc" name="bank">
+                            <label for="icbc"><img src="/images/rewardtask/icbc.gif" alt="icbc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "abc" name="bank">
+                            <label for="abc"><img src="/images/rewardtask/abc.gif" alt="abc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "ibc" name="bank">
+                            <label for="ibc"><img src="/images/rewardtask/ibc.gif" alt="ibc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "sbc" name="bank">
+                            <label for="sbc"><img src="/images/rewardtask/sbc.gif" alt="sbc" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="bank-item">
+                            <input type="radio" id= "alipay" name="bank">
+                            <label for="alipay"><img src="/images/rewardtask/alipay.gif" alt="alipay" class="bank" width="150" height="30" />
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </li>
+            </ul>
+        </form>
         <div class="next-step">
             <a href="/rewardtask/step-2" class="btn last">上一步</a>
             <a href="/rewardtask/step-4" class="btn next">下一步</a>
