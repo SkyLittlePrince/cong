8e379b61a86e00b2ec025d88588d18917aaf4ac6
cyrilzhao <cyrilzhao@qq.com>
2015-04-08 12:29:19 +0800

    modified grunt file

--- app/views/auctions/step-4.blade.php
+++ app/views/auctions/step-4.blade.php
@@ -23,16 +23,7 @@
                     <label class="label">任务主题：</label>
                     <span>广州紫睿科技有限公司</span>
                 </li>
-                <li>
-                    <label class="label">赏金价格：</label>
-                    <span>100元</span>
-                    <span>20个铜币</span>
-                </li>
-                <li>
-                    <label class="label">请预付：</label>
-                    <span class="prepay">100元</span>
-                    <span class="prepay">20个铜币</span>
-                </li>
+                
             </ul>
         </form>
         <div class="next-step">
