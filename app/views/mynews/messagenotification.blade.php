d4e9525188b535aa5c7c0bb4db3d92f5f7bf8eb2
yuanzm <1229084233@qq.com>
2015-04-12 15:24:00 +0800

    complete message notification page

--- app/views/mynews/messagenotification.blade.php
+++ app/views/mynews/messagenotification.blade.php
@@ -12,24 +12,102 @@
 @include('components.header')
 <div id="main">
     @include('components.task-banner')
-    @include('components.employerleft')
+    @include('components.mynewsleft')
    	@include('components.tradingcenterheader')
-   	<div class="mynews-component messagenotification">
+   	<div class="mynews-component message-notification">
    		<div class="message-wrapper">
 	   		<div class="banner">
 	   			<span class="title">广告/活动(<span>2</span>)</span>
 	   			<span class="status">未读/全部(<span>3</span>)</span>
 	   		</div>
 	   		<div class="operate">
-	   			
+                <label for="check-box">
+                    <input type="checkbox" name="message" value="all" class="select-all">
+                </label>
+                <div class="col-right">
+    	   			<input type="button" value="标记所有为已读" class="mark-all" style="margin-left: 0;">
+                    <input type="button" value="删除所有" class="delete-all">
+                    <input type="button" value="清空所有" class="clear-all">
+                </div>
 	   		</div>
 	   		<div class="messages">
 	   			<ul>
-	   				<li class="one-message"></li>
+	   				<li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
+                    <li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
+                    <li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
+                    <li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
+                    <li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
+                    <li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
+                    <li class="one-message">
+                        <label>
+                            <input type="checkbox" name="message" value="message1">
+                        </label>             
+                        <div class="col-right">
+                            你秀才艺我给钱,现在正式接收报名!
+                        </div>
+                    </li>
 	   			</ul>
 	   		</div>
 	   		<div class="pagination">
-	   			
+                <div class="right to-page">
+                    <p>
+                        共<span class="page-count">3</span>页,到第<input type="text" >页
+                        <input type="button" value="确定">
+                    </p>
+                </div>
+	   			<div class="page-num right">
+                    <a href="#">
+                        <img src="/images/icon/icon-arrow-left.png" alt="icon-left" width="28" height="28" />         
+                    </a>
+                    <span class="num active">1</span>
+                    <span class="num">2</span>
+                    <span class="num">3</span>
+                    <a href="#">
+                        <img src="/images/icon/icon-arrow-right.png" alt="icon-right" width="28" height="28" />
+                    </a>
+                </div>
 	   		</div>
    		</div>
    	</div>
