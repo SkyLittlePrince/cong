670b747bb9be7094b34f4563abbef0726f59f141
cyrilzhao <cyrilzhao@qq.com>
2015-04-13 23:22:14 +0800

    complete change-password page

--- app/views/tradingCenter/account/change-password.blade.php
+++ app/views/tradingCenter/account/change-password.blade.php
@@ -18,7 +18,31 @@
 		<h3>修改登录密码</h3>
 		<hr />		
 		<div class="content-main">
-
+			<div class="content-row" style="margin-bottom: 29px;">
+				<img src="/images/tradingcenter/icon/2.png" width="20" height="20" />
+				<span class="warning">安全提醒：请妥善保管密码！丛丛网工作人员不会以任何理由向你索取密码</span>
+			</div>	
+			<div class="content-row">
+				<label class="label" for="email">当前密码：</label>
+				<div class="content-input">
+					<input type="text" id="email" name="email" />
+				</div>
+			</div>
+			<div class="content-row">
+				<label class="label" for="email">设置新密码：</label>
+				<div class="content-input">
+					<input type="text" id="email" name="email" />
+				</div>
+			</div>
+			<div class="content-row">
+				<label class="label" for="email">确认新密码：</label>
+				<div class="content-input">
+					<input type="text" id="email" name="email" />
+				</div>
+			</div>
+			<div class="content-row">
+                <a href="" class="btn" id="change-password-confirm-btn">确定</a>
+			</div>
 		</div>
 	</div>
 @stop
