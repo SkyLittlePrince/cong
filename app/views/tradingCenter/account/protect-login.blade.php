a315b4f00b5a2c6f8f02397ffee42a8e5edbddfa
cyrilzhao <cyrilzhao@qq.com>
2015-04-14 13:43:19 +0800

    change mode

--- app/views/tradingCenter/account/protect-login.blade.php
+++ app/views/tradingCenter/account/protect-login.blade.php
@@ -14,7 +14,20 @@
 @stop
 
 @section('tradingCenter-content')
-	
+	<div class="protect-login-content tradingCenter-content">
+		<h3>账号登录保护</h3>
+		<hr />		
+		<div class="content-main">
+			<div class="content-row" style="margin-bottom: 29px;">
+				<img src="/images/tradingcenter/icon/2.png" width="20" height="20" />
+				<span class="warning">当你设置账号登录保护后，任何异常的登录行为，都不会被允许，只有在经过您的手机验证通过后方可登录成功</span>
+			</div>	
+			<div class="content-row">
+				<span>状态：未开通</span>
+                <a href="" class="btn" id="protect-login-confirm-btn">确定</a>
+			</div>
+		</div>
+	</div>
 @stop
 
 @section('js')
