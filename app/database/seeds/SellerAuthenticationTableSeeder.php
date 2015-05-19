206fa1fc91ffcccceddbf1f0a0319ba239e41b0d
cyrilzhao <cyrilzhao@qq.com>
2015-05-20 00:03:14 +0800

    fix bug

--- app/database/seeds/SellerAuthenticationTableSeeder.php
+++ app/database/seeds/SellerAuthenticationTableSeeder.php
@@ -9,8 +9,8 @@ class SellerAuthenticationTableSeeder extends Seeder {
 			"name" => "无名氏",
 			"credit_id" => "460035199004280312",
 			"gender" => 1,
-			"credit_front" => "aaa.jpg",
-			"credit_behind" => "bbb.jpg",
+			"credit_front" => "http://7xj0sp.com1.z0.glb.clouddn.com/aaa.jpg",
+			"credit_behind" => "http://7xj0sp.com1.z0.glb.clouddn.com/bbb.jpg",
 			"address" => "广州市番禺区大学城外环西路100号广州国家集成电路基地",
 			"phone" => "15902084760"
 		]);
@@ -20,8 +20,8 @@ class SellerAuthenticationTableSeeder extends Seeder {
 			"name" => "脑残无名氏",
 			"credit_id" => "460035198805160576",
 			"gender" => 1,
-			"credit_front" => "111.jpg",
-			"credit_behind" => "222.jpg",
+			"credit_front" => "http://7xj0sp.com1.z0.glb.clouddn.com/111.jpg",
+			"credit_behind" => "http://7xj0sp.com1.z0.glb.clouddn.com/222.jpg",
 			"address" => "广州市番禺区大学城外环西路100号广州国家集成电路基地",
 			"phone" => "15902084650"
 		]);
