06902312418375eaef438c0d78246503cdeb42f2
cyrilzhao <cyrilzhao@qq.com>
2015-05-19 21:07:23 +0800

    订单评价

--- app/views/tradingCenter/seller-center/indent-evaluation.blade.php
+++ app/views/tradingCenter/seller-center/indent-evaluation.blade.php
@@ -12,12 +12,13 @@
 	@parent
 	<div id="main">
 		<div class="indent-top">
-			<p>订单号WX0386526238736</p>
-			<p>2014.12.25</p>
-			<p>卖家：Vivine</p>
-			<p>学号FC634805</p>
+			<p>订单号：{{{ $indent->id }}}</p>
+			<p>下单时间：{{{ $indent->created_at }}}</p>
+			<p>卖家：{{{ $seller->username }}}</p>
+			<p>店铺名称：{{{ $shop->name }}}</p>
 		</div>
 		<div class="indent-center">
+			<input type="hidden" id="product-id" value="{{{$product->id}}}" />
 			<div class="center-left">
 				<img src="/images/tradingcenter/buyer/comment.png" />
 			</div>
@@ -31,39 +32,12 @@
 					<img src="/images/tradingcenter/icon/star.png" />
 					<img src="/images/tradingcenter/icon/star.png" />
 				</div>
-				<!-- <div class="point">
-					<span class="word2">卖家的服务态度：</span>
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-				</div>
-				<div class="point">
-					<span class="word2">卖家的工作效率：</span>
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-					<img src="/images/tradingcenter/icon/star.png" />
-				</div> -->
-				<!-- <p class="word1 point">提示：请根据对商品的满意程度给商品进行评价或添加内容描述。</p> -->
-				<!-- <div  class="point">
-					<span class="word2">评价内容：</span>
-					<input type="radio" class="assess" id="best"/><span>好评</span>
-					<input type="radio" class="assess" id="medium"/><span>中评</span>
-					<input type="radio" class="assess" id="worse"/><span>差评</span>
-				</div>-->
-					<textarea name="content" id="content"></textarea>
-
-					<input type="button" class="confirm" id="confirm" value="确认">
+				<textarea name="content" id="content"></textarea>
+				<input type="button" class="confirm" id="confirm" value="确认">
 			</div>
 			<div class="center-foot">
 				<p>提示：如果对此次交易特别满意，还可以给卖家打赏哦！</p>
 				<input type="text" class="reward-enter" id="rewardenter" placeholder="输入赏金">
-				<input type="radio" class="evaluate" name="best" id="best"/><span>好评</span>
-				<input type="radio" class="evaluate" name="medium" id="medium"/><span>中评</span>
-				<input type="radio" class="evaluate" name="worse" id="worse"/><span>差评</span>
 				<input type="button" class="reward" id="reward" value="打赏">
 			</div>
 		</div>
