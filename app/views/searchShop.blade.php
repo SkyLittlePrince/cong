69e854ea8832a03f438bc1f2621733fec7213086
cyrilzhao <cyrilzhao@qq.com>
2015-05-20 16:56:26 +0800

    新增没有搜索到店铺时候的信息

--- app/views/searchShop.blade.php
+++ app/views/searchShop.blade.php
@@ -20,27 +20,33 @@
             </ul>
         </div>
         <div class="shops">
-        	@foreach ($shops as $shop)
-        	<a href="/trading-center/seller/seller-store?shop_id={{{ $shop->id }}}" target="_blank">
+        	@if (count($shops) != 0)
+	        	@foreach ($shops as $shop)
+	        	<a href="/trading-center/seller/seller-store?shop_id={{{ $shop->id }}}" target="_blank">
+		        	<div class="one-shop">
+			    		<div class="shop-info shop-component">
+			    			<input type="hidden" value="{{{ $shop->id }}}">
+			    			<img class="shop-avatar" src="{{{ $shop->avatar }}}" width="40" height="40" />
+			    			<span>{{{ $shop->name }}}</span>
+			    		</div>
+						<div class="shop-desc shop-component">
+							{{{ $shop->description }}}
+						</div>
+						<div class="shop-tags shop-component">
+							{{{ $shop->tagName }}}
+						</div>
+						<div class="shop-score shop-component">
+							{{{ $shop->aScore }}}
+						</div>
+						<div class="clear"></div>
+			    	</div>
+			    </a>
+	        	@endforeach
+	        @else
 	        	<div class="one-shop">
-		    		<div class="shop-info shop-component">
-		    			<input type="hidden" value="{{{ $shop->id }}}">
-		    			<img class="shop-avatar" src="{{{ $shop->avatar }}}" width="40" height="40" />
-		    			<span>{{{ $shop->name }}}</span>
-		    		</div>
-					<div class="shop-desc shop-component">
-						{{{ $shop->description }}}
-					</div>
-					<div class="shop-tags shop-component">
-						{{{ $shop->tagName }}}
-					</div>
-					<div class="shop-score shop-component">
-						{{{ $shop->aScore }}}
-					</div>
-					<div class="clear"></div>
-		    	</div>
-		    </a>
-        	@endforeach
+	        		很抱歉，没有找到您需要的店铺
+	        	</div>
+	        @endif
         </div>
         @if (count($shops) < $shops->getTotal())
             {{ $shops->links() }}
