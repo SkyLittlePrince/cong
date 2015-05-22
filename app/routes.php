711726c840f4f7cb2b383e976954a4ebd0e42879
turtleLin <930030895@qq.com>
2015-05-22 13:56:41 +0800

    add userAuthentication

--- app/routes.php
+++ app/routes.php
@@ -356,6 +356,8 @@ Route::group(array('prefix' => 'admin','before' => 'auth.user.isAdmin'),function
 	Route::post('deleteProduct','AdminController@deleteProduct');
 	Route::post('updateIndent','AdminController@updateIndent');
 	Route::post('updateProduct','ProductController@updateProduct');
+	Route::post('passAuthentication','SellerAuthenticationController@pass');
+	Route::post('failAuthentication','SellerAuthenticationController@fail');
 });
 
 
