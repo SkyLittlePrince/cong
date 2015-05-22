c684997cce8f2142ae1dac5b1eb32ee09e47407c
tangwaikei <827701764@qq.com>
2015-05-22 16:46:05 +0800

    update the limit of the description of the productcontroller.php

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -36,7 +36,7 @@ class ProductController extends \BaseController {
 				'avatar' => $avatar
 			),
 			array(
-				'intro'=>'between : 2,500',
+				'intro'=>'between : 2,501',
 				'price' => 'numeric',
 				'avatar' =>'url'
 			),
@@ -101,7 +101,7 @@ class ProductController extends \BaseController {
 				'avatar' => $avatar
 			),
 			array(
-				'intro'=>'between : 2,500',
+				'intro'=>'between : 2,502',
 				'price' => 'numeric',
 				'avatar' =>'url'
 			),
